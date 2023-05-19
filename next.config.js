ac9ae42af8026109a386883a6d00dd91548c7860
tsots <mosalatsotetsi@gmail.com>
2023-05-19 05:21:04 +0200

    changed to full mdx

--- next.config.js
+++ next.config.js
@@ -1,10 +1,21 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === "production";
 
+module.exports = nextConfig;
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+  pageExtensions: ["js", "jsx", "md", "mdx"],
   output: "export",
   assetPrefix: isProd ? "/chingy" : undefined,
   basePath: "/chingy",
+  experimental: {
+    appDir: true,
+    mdxRs: true,
+  },
 };
 
-module.exports = nextConfig;
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
+});
+module.exports = withMDX(nextConfig);
